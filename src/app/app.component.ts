e5e0b50fc44a494a01a0f843a132c97a24e10e32
Zdeněk Němec <xzdeneknemec@outlook.cz>
2025-05-15 22:10:23 +0200

    clear app.component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,8 +5,9 @@ import { RouterOutlet } from '@angular/router';
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'zajacovic';
+  protected test() {
+    console.log('test');
+  }
 }
