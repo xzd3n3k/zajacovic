97f8483d5f3dd2d2eb6d8e7e13ce58a36af0ce05
Zdeněk Němec <xzdeneknemec@outlook.cz>
2025-05-16 16:25:03 +0200

    add home screen text

--- src/app/home-screen/home-screen.component.ts
+++ src/app/home-screen/home-screen.component.ts
@@ -1,14 +1,15 @@
 import { Component } from '@angular/core';
-import {NavbarComponent} from '../components/navbar/navbar.component';
+import {HeadingComponent} from '../components/heading/heading.component';
 
 @Component({
   selector: 'zaj-home-screen',
   imports: [
-    NavbarComponent
+    HeadingComponent
   ],
   templateUrl: './home-screen.component.html',
   styleUrl: './home-screen.component.scss'
 })
 export class HomeScreenComponent {
-
+  text1 = 'Specializujeme se na zakázkovou výrobu nerezových dílů pro motorsport a úpravy vozidel. Vyrábíme výfuky, intercoolery, potrubí a další specifické komponenty na míru. Svařujeme metodami TIG a CO2 – s důrazem na preciznost a funkční design.';
+  text2 = 'Specializujeme se na zakázkovou výrobu nerezových dílů pro motorsport a úpravy vozidel. Na míru navrhujeme a vyrábíme výfukové systémy, intercoolery, potrubí a další technické komponenty. Pracujeme metodami TIG a CO2, s důrazem na precizní zpracování, funkčnost a čistý design.';
 }
