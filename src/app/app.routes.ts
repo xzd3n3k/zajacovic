0e1b9ce2a9ba227e01abd2cbb7581bafd6742477
Zdeněk Němec <xzdeneknemec@outlook.cz>
2025-05-17 12:30:11 +0200

    add favicon, add tab title, add logo icon

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,13 +3,15 @@ import {HomeScreenComponent} from './home-screen/home-screen.component';
 import {AboutScreenComponent} from './about-screen/about-screen.component';
 
 export const routes: Routes = [
-  {'path': '', redirectTo: '/home', pathMatch: 'full'},
+  {path: '', redirectTo: '/home', pathMatch: 'full'},
   {
-    'path': 'home',
+    path: 'home',
     component: HomeScreenComponent,
+    title: 'Zajacovič Exhaust Systems'
   },
   {
-    'path': 'about',
+    path: 'about',
     component: AboutScreenComponent,
+    title: 'Zajacovič Exhaust Systems'
   }
 ];
