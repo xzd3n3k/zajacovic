3f78ff758e0b59150e27509be14c8819afc22edf
Zdeněk Němec <xzdeneknemec@outlook.cz>
2025-05-17 23:40:30 +0200

    home screen component code styling. add height 100

--- src/app/home-screen/home-screen.component.ts
+++ src/app/home-screen/home-screen.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
-import {HeadingComponent} from '../components/heading/heading.component';
-import {TranslatePipe} from '@ngx-translate/core';
+import { HeadingComponent } from '../components/heading/heading.component';
+import { TranslatePipe } from '@ngx-translate/core';
 
 @Component({
   selector: 'zaj-home-screen',
   imports: [
     HeadingComponent,
-    TranslatePipe
+    TranslatePipe,
   ],
   templateUrl: './home-screen.component.html',
   styleUrl: './home-screen.component.scss'
