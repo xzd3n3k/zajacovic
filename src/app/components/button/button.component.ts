eb77e3d66690856997ea8f906aa7634481d8e091
Zdeněk Němec <xzdeneknemec@outlook.cz>
2025-05-17 22:57:43 +0200

    add element field to button so it can pass itself as HTMLElement

--- src/app/components/button/button.component.ts
+++ src/app/components/button/button.component.ts
@@ -1,4 +1,4 @@
-import {Component, input} from '@angular/core';
+import {Component, ElementRef, inject, input} from '@angular/core';
 import {ButtonVariant} from '../../models/button-variant.type';
 import {ButtonSize} from '../../models/button-size.type';
 
@@ -12,4 +12,12 @@ export class ButtonComponent {
   readonly variant = input<ButtonVariant>('primary');
   readonly size = input<ButtonSize>('normal');
   readonly disabled = input<boolean>(false);
+
+  readonly element: HTMLElement;
+
+  private readonly hostElementRef = inject(ElementRef);
+
+  constructor() {
+    this.element = this.hostElementRef.nativeElement;
+  }
 }
