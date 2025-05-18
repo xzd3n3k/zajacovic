ddb04e1c78484278b4d4759fa561d9971e1fddcc
Zdeněk Němec <xzdeneknemec@outlook.cz>
2025-05-18 14:43:41 +0200

    create post model and post component

---
+++ src/app/components/post/post.component.ts
@@ -0,0 +1,20 @@
+import {Component, input, viewChild} from '@angular/core';
+import {DialogComponent} from '../dialog/dialog.component';
+import {Post} from '../../shared/models/post.model';
+
+@Component({
+  selector: 'zaj-post',
+  imports: [
+    DialogComponent
+  ],
+  templateUrl: './post.component.html',
+  styleUrl: './post.component.scss'
+})
+export class PostComponent {
+  readonly post = input.required<Post>();
+  private readonly dialog = viewChild.required<DialogComponent>(DialogComponent);
+
+  protected openDialog() {
+    this.dialog().open();
+  }
+}
