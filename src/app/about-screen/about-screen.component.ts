8a76916a14410b7a2ec9185a789f10ed959574aa
Zdeněk Němec <xzdeneknemec@outlook.cz>
2025-05-18 11:50:56 +0200

    add background and improve about screen

--- src/app/about-screen/about-screen.component.ts
+++ src/app/about-screen/about-screen.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import {TranslatePipe} from '@ngx-translate/core';
+import {HeadingComponent} from '../components/heading/heading.component';
 
 @Component({
   selector: 'zaj-about-screen',
   imports: [
-    TranslatePipe
+    TranslatePipe,
+    HeadingComponent
   ],
   templateUrl: './about-screen.component.html',
   styleUrl: './about-screen.component.scss'
