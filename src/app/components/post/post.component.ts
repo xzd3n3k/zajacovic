cc8a4b96f35ac2a27e71ec6f50700ff94792fe82
Zdeněk Němec <xzdeneknemec@outlook.cz>
2025-05-18 15:33:41 +0200

    add translations for posts

--- src/app/components/post/post.component.ts
+++ src/app/components/post/post.component.ts
@@ -1,11 +1,13 @@
 import {Component, input, viewChild} from '@angular/core';
 import {DialogComponent} from '../dialog/dialog.component';
 import {Post} from '../../shared/models/post.model';
+import {TranslatePipe} from '@ngx-translate/core';
 
 @Component({
   selector: 'zaj-post',
   imports: [
-    DialogComponent
+    DialogComponent,
+    TranslatePipe
   ],
   templateUrl: './post.component.html',
   styleUrl: './post.component.scss'
