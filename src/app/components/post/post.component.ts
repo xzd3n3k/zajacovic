4e03cbfefc978cc300e63fadad62eb299dc207b5
Zdeněk Němec <xzdeneknemec@outlook.cz>
2025-05-20 16:17:31 +0200

    update post preview text width on image load

--- src/app/components/post/post.component.ts
+++ src/app/components/post/post.component.ts
@@ -1,4 +1,4 @@
-import {Component, input, viewChild} from '@angular/core';
+import {Component, input, signal, viewChild} from '@angular/core';
 import {DialogComponent} from '../dialog/dialog.component';
 import {Post} from '../../shared/models/post.model';
 import {TranslatePipe} from '@ngx-translate/core';
@@ -16,7 +16,13 @@ export class PostComponent {
   readonly post = input.required<Post>();
   private readonly dialog = viewChild.required<DialogComponent>(DialogComponent);
 
+  protected readonly textWidth = signal(0);
+
   protected openDialog() {
     this.dialog().open();
+  }
+
+  updateMaxTextWidth(imgRef: HTMLImageElement) {
+    this.textWidth.set(imgRef.clientWidth);
   }
 }
